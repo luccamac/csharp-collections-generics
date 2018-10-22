dc75dc3e9b0c740e4c0daf0a0c239212780371a5
Lucca Machado <lucca.santos@acad.pucrs.br>
2018-10-22 12:24:01 -0300

    módulo 3 sobre arrays completo

---
+++ AcmeApp/Acme.Biz/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz
+{
+    class Product
+    {
+        public Product()
+        {
+            string[] colorOptions = { "Red", "Espresso", "White", "Navy" };
+            var redIndex = Array.IndexOf(colorOptions, "Red");
+            colorOptions.SetValue("Blue", 3);
+        }
+
+        public Product(int productId, 
+                       string productName,
+                       string description): this()
+        {
+
+        }
+    }
+}
